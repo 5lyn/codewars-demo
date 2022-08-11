1626baae02996d20173280b2dd3199d8b50acd01
Glyn <glyncolls@outlook.com>
2022-08-11 16:32:31 +0100

    Completed solution 3 of codewars

--- js/solution003.js
+++ js/solution003.js
@@ -1,1 +1,11 @@
-console.log("some more code");
+//ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+//If the function is passed a valid PIN string, return true, else return false.
+
+function validatePIN(pin) {
+  //return true or false
+  return (/^\d+$/.test(pin) && pin.length === 4) ||
+    (/^\d+$/.test(pin) && pin.length === 6)
+    ? true
+    : false;
+}
